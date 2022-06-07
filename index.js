b4a2221f0415db4f68cc955fe8b7db4d43f66d02
benjamin creniaut <creniaut.benjamin@gmail.com>
2022-06-07 16:23:15 +0200

    add cors options

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const event = require("./event");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 app.get("/event", (req, res) => {
   res.json(event);
