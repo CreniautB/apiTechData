1077b64d0b3a85113b900974211f9257078f4a4a
benjamin creniaut <creniaut.benjamin@gmail.com>
2022-06-07 17:02:24 +0200

    add video script

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const fetch = require("fetch");
 const app = express();
 const event = require("./event");
 const video = require("./video");
 const axios = require("axios");
 const xml2js = require("xml2js");
+const fs = require("fs");
 
 const corsOptions = {
   origin: "*",
@@ -21,35 +22,15 @@ app.get("/event", (req, res) => {
 
 app.get("/video", (req, res) => {
   const url = "https://vimeo.com/techdatafrance/videos/rss";
-
-  fetchData(url).then((res) => {
-    xml2js.parseString(res.data, (err, result) => {
+  axios(url).then((reponse) => {
+    xml2js.parseString(reponse.data, (err, result) => {
       if (err) {
         throw err;
       }
-
-      // `result` is a JavaScript object
-      // convert it to a JSON string
-      const json = JSON.stringify(result, null, 4);
-
-      // log JSON string
-      let videoVimeoData = JSON.stringify(result.rss.channel[0].item);
+      let videoVimeoData = result.rss.channel[0].item;
       res.json(videoVimeoData);
     });
   });
-
-  async function fetchData(url) {
-    console.log("Crawling data...");
-    // make http call to url
-    let response = await axios(url).catch((err) => console.log(err));
-
-    if (response.status !== 200) {
-      console.log("Error occurred while fetching data");
-      return;
-    }
-    return response;
-  }
-  res.json(video);
 });
 
 app.get("/", (req, res) => {
